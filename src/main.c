dcc6d7af8b3d5b2e852e8e39e31d087f1ccbac29
amarpersaud <tehspartaa@gmail.com>
2021-05-16 16:00:43 -0400

    Add basic code to open map, intelligence and output files

--- src/main.c
+++ src/main.c
@@ -3,9 +3,33 @@
 #include "ant.h"
 #include "config.h"
 
+#define MAPFILENAME "mapfile.txt"
+#define INTELLIGENCEFILENAME "intelligencefile.txt"
+#define OUTPUTFILENAME "outputfile.txt"
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	FILE* mapFile;
+	FILE* intelligenceFile;
+	FILE* outputFile;
+	
+	/*Load map, intelligence, and output files*/
+	if((mapFile = fopen(MAPFILENAME, "r")) == NULL){
+		printf("Failed to open map file, '%s'", MAPFILENAME);
+		exit(1);
+	}	
+	if((intelligenceFile = fopen(INTELLIGENCEFILENAME, "r")) == NULL){
+		printf("Failed to open intelligence file, '%s'", INTELLIGENCEFILENAME);
+		exit(1);
+	}	
+	if((outputFile = fopen(OUTPUTFILENAME, "w")) == NULL){
+		printf("Failed to open output file, '%s'", OUTPUTFILENAME);
+		exit(1);
+	}
+	
+	
 	
 	return 0;
 }
