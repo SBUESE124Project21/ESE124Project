36271e7eb627db205cb073c96bb90bc41e5f9913
amarpersaud <tehspartaa@gmail.com>
2021-05-16 15:55:21 -0400

    Add command constants to config.h

--- src/config.h
+++ src/config.h
@@ -5,14 +5,29 @@
 #define DIR_LEFT 2 
 #define DIR_DOWN 3
 
-#define TILE_SPACE '*'
-#define TILE_WALL '|'
-#define TILE_GOLD '$'
+#define TILE_SPACE ' '
+#define TILE_WALL '*'
+#define TILE_GOLD '@'
 #define TILE_PHEROMONE '?'
-#define TILE_OUTSIDE '@'
+#define TILE_OUTSIDE '_'
 
 #define MAX_STACKSIZE 100
 
 #define MAX_SIZE 100
 #define MAX_ENERGY 100
 
+#define C_MARK 0
+#define C_MOVE_F 1
+#define C_MOVE_B 2
+#define C_MOVE_L 3
+#define C_MOVE_R 4
+#define C_CWF 5
+#define C_CWB 6
+#define C_CWL 7
+#define C_CWR 8
+#define C_PUSH 9
+#define C_POP 10
+#define C_PEEK 11
+#define C_CLEAR 12
+#define C_BJPI 13
+#define C_CJPI 14
