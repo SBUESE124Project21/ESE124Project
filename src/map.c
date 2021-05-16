719106381485a5e3edbcf01d4eef2827028a4f39
amarpersaud <tehspartaa@gmail.com>
2021-05-15 20:18:58 -0400

    Move map function implementation to map.c

---
+++ src/map.c
@@ -0,0 +1,27 @@
+#include "map.h"
+
+map* makeMap(int width, int height){
+	map* m = (map*)calloc(1, sizeof(map));
+	
+	m->width = width;
+	m->height = height;
+	
+	m->data = (int**)calloc(width, sizeof(int*));
+	
+	int i;
+	for(i = 0; i < width; i++){
+		m->data[i] = (int*)calloc(height, sizeof(int));
+	}
+	
+	return m;
+}
+
+void freeMap(map* m){
+	int i;
+	
+	for(i = 0; i < m->width; i++){
+		free(m->data[i]);
+	}
+	free(m->data);
+	free(m);
+}
--- src/map.h
+++ src/map.h
@@ -6,28 +6,5 @@ struct map{
 	int** data;
 };
 
-map* makeMap(int width, int height){
-	map* m = (map*)calloc(1, sizeof(map));
-	
-	m->width = width;
-	m->height = height;
-	
-	m->data = (int**)calloc(width, sizeof(int*));
-	
-	int i;
-	for(i = 0; i < width; i++){
-		m->data[i] = (int*)calloc(height, sizeof(int));
-	}
-	
-	return m;
-}
-
-freeMap(map* m){
-	int i;
-	
-	for(i = 0; i < m->width; i++){
-		free(m->data[i]);
-	}
-	free(m->data);
-	free(m);
-}
+map* makeMap(int width, int height);
+void freeMap(map* m);
